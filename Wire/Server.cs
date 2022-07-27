8b1c45666e9b15771a4e116ef7c78fbab332fa1f
theoistic <theo@theoistic.com>
2022-07-27 21:23:36 +0100

    Multithreaded server

--- Wire/Server.cs
+++ Wire/Server.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -7,29 +8,61 @@ namespace Wire
 {
     public class WireHTTPServer : IDisposable
     {
-       
-        private Thread _serverThread;
-        private HttpListener _listener;
-        private int _port;
+        private readonly HttpListener _listener;
+        private readonly Thread _listenerThread;
+        private readonly Thread[] _workers;
+        private readonly ManualResetEvent _stop, _ready;
+        private Queue<HttpListenerContext> _queue;
 
-        public int Port
-        {
-            get { return _port; }
-            private set { }
-        }
+        public int Port { get; private set; }
 
-        public WireHTTPServer(int port = 80)
+        public WireHTTPServer(int port = 80, int maxThreads = 8)
         {
-            this.Initialize(port);
-        }
-
-        public WireHTTPServer()
-        {
-            TcpListener l = new TcpListener(IPAddress.Loopback, 0);
-            l.Start();
-            int port = ((IPEndPoint)l.LocalEndpoint).Port;
-            l.Stop();
-            this.Initialize(port);
+            _workers = new Thread[maxThreads];
+            _queue = new Queue<HttpListenerContext>();
+            _stop = new ManualResetEvent(false);
+            _ready = new ManualResetEvent(false);
+            _listener = new HttpListener();
+            _listenerThread = new Thread(() => {
+                while (_listener.IsListening) {
+                    var context = _listener.BeginGetContext((x) => {
+                        try {
+                            lock (_queue) {
+                                _queue.Enqueue(_listener.EndGetContext(x));
+                                _ready.Set();
+                            }
+                        }
+                        catch { return; }
+                    }, null);
+                    if (0 == WaitHandle.WaitAny(new[] { _stop, context.AsyncWaitHandle })) return;
+                }
+            });
+            Port = port;
+            _listener.Prefixes.Add(String.Format(@"http://+:{0}/", Port));
+            _listener.Start();
+            _listenerThread.Start();
+            for (int i = 0; i < _workers.Length; i++) {
+                _workers[i] = new Thread(async () => {
+                    WaitHandle[] wait = new[] { _ready, _stop };
+                    while (0 == WaitHandle.WaitAny(wait)) {
+                        HttpListenerContext context;
+                        lock (_queue) {
+                            if (_queue.Count > 0)
+                                context = _queue.Dequeue();
+                            else {
+                                _ready.Reset();
+                                continue;
+                            }
+                        }
+                        try {
+                            await API.Resolve(API.FromHttpListener(context));
+                            context.Response.Close();
+                        }
+                        catch (Exception e) { Console.Error.WriteLine(e); }
+                    }
+                });
+                _workers[i].Start();
+            }
         }
 
         public void Wait()
@@ -40,41 +73,13 @@ namespace Wire
 
         public void Stop()
         {
-            _serverThread.Abort();
+            _stop.Set();
+            _listenerThread.Join();
+            foreach (Thread worker in _workers)
+                worker.Join();
             _listener.Stop();
         }
-        
-        private async void Listen()
-        {
-            _listener = new HttpListener();
-            _listener.Prefixes.Add("http://*:" + _port.ToString() + "/");
-            //_listener.Prefixes.Add("https://*:" + _port.ToString() + "/");
-            _listener.Start();
-            while (true)
-            {
-                try
-                {
-                    HttpListenerContext context = _listener.GetContext();
-                    await API.Resolve(API.FromHttpListener(context));
-                    context.Response.Close();
-                }
-                catch (Exception ex)
-                {
 
-                }
-            }
-        }
-
-        private void Initialize(int port)
-        {
-            this._port = port;
-            _serverThread = new Thread(this.Listen);
-            _serverThread.Start();
-        }
-
-        public void Dispose()
-        {
-            this.Stop();
-        }
+        public void Dispose() => Stop();
     }
 }
