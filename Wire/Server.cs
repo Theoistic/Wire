5f3e3d43f5f6f61f30d64bd0a1323f8cb9794386
theoistic <theo@theoistic.com>
2022-07-27 21:38:53 +0100

    updated server for threadpool

--- Wire/Server.cs
+++ Wire/Server.cs
@@ -10,27 +10,23 @@ namespace Wire
     {
         private readonly HttpListener _listener;
         private readonly Thread _listenerThread;
-        private readonly Thread[] _workers;
-        private readonly ManualResetEvent _stop, _ready;
-        private Queue<HttpListenerContext> _queue;
+        private readonly ManualResetEvent _stop;
 
         public int Port { get; private set; }
 
         public WireHTTPServer(int port = 80, int maxThreads = 8)
         {
-            _workers = new Thread[maxThreads];
-            _queue = new Queue<HttpListenerContext>();
             _stop = new ManualResetEvent(false);
-            _ready = new ManualResetEvent(false);
             _listener = new HttpListener();
             _listenerThread = new Thread(() => {
                 while (_listener.IsListening) {
                     var context = _listener.BeginGetContext((x) => {
                         try {
-                            lock (_queue) {
-                                _queue.Enqueue(_listener.EndGetContext(x));
-                                _ready.Set();
-                            }
+                            var _context = _listener.EndGetContext(x);
+                            ThreadPool.QueueUserWorkItem(new WaitCallback(async x => {
+                                await API.Resolve(API.FromHttpListener(_context));
+                                _context.Response.Close();
+                            }));
                         }
                         catch { return; }
                     }, null);
@@ -41,28 +37,6 @@ namespace Wire
             _listener.Prefixes.Add(String.Format(@"http://+:{0}/", Port));
             _listener.Start();
             _listenerThread.Start();
-            for (int i = 0; i < _workers.Length; i++) {
-                _workers[i] = new Thread(async () => {
-                    WaitHandle[] wait = new[] { _ready, _stop };
-                    while (0 == WaitHandle.WaitAny(wait)) {
-                        HttpListenerContext context;
-                        lock (_queue) {
-                            if (_queue.Count > 0)
-                                context = _queue.Dequeue();
-                            else {
-                                _ready.Reset();
-                                continue;
-                            }
-                        }
-                        try {
-                            await API.Resolve(API.FromHttpListener(context));
-                            context.Response.Close();
-                        }
-                        catch (Exception e) { Console.Error.WriteLine(e); }
-                    }
-                });
-                _workers[i].Start();
-            }
         }
 
         public void Wait()
@@ -75,8 +49,6 @@ namespace Wire
         {
             _stop.Set();
             _listenerThread.Join();
-            foreach (Thread worker in _workers)
-                worker.Join();
             _listener.Stop();
         }
 
